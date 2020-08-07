ca0ad8a660c432726170aac1801f952810c1096c
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-07 02:04:32 -0300

    Página, componente & customização do form + lista

---
+++ src/components/FormTutors/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PageHeader from '../PageHeader'
+
+const FormTutors = () => {
+  return (
+    <React.Fragment>
+      <div className='heading container'>
+        <PageHeader display='Que incrível que você quer dar aulas'></PageHeader>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default FormTutors
