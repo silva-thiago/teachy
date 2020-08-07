a1900762e6459621a4731218bbf6a58bbd8bb624
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-07 02:00:14 -0300

    Tag html para texto subtituída

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,9 +5,9 @@ const Error404 = () => {
     <React.Fragment>
       <div className='error404-container'>
         <div className='text-center'>
-          <h1 className='error404-title'>404</h1>
+          <p className='error404-title'>404</p>
           <div className='error404-position'>
-            <h2 className='error404-description'>This page could not be found</h2>
+            <p className='error404-description'>This page could not be found</p>
           </div>
         </div>
       </div>
